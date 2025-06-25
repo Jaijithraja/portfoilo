38d6135153d4d1fcdae7b0580e8b7328d21194e6
Jaijithraja <jaijith1808@gmail.com>
2025-06-25 16:27:56 +0530

    Initial commit: portfolio website

---
+++ main.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('nav ul li a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const target = document.getElementById(targetId);
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - 60,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
